8a98fa909a8b102c8ed1d531551b34e31c9d6840
intechcompany <intechcompany.association@gmail.com>
2023-11-20 15:09:20 +0700

    Add navbar in the In tech computerizer

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,12 +7,14 @@ export default function Navbar() {
   return (
     <header className="section py-10">
       <nav className="flex items-center justify-between">
-        <div className="flex items-center gap-2">
-          <img src={logo} alt="logo" width={90} height={90} />
-          <h3 className="text-black-1 font-bold text-base">
-            IN TECH <br /> COMPUTERIZER
-          </h3>
-        </div>
+        <Link to={"/"}>
+          <div className="flex items-center gap-2">
+            <img src={logo} alt="logo" width={90} height={90} />
+            <h3 className="text-black-1 font-bold text-base">
+              IN TECH <br /> COMPUTERIZER
+            </h3>
+          </div>
+        </Link>
         <div className="hidden md:block">
           <div className="flex gap-5">
             <Link to={"/about"}>
